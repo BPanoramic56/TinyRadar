df044783335336a729fd1f701a23b9c13978d00f
brunogomespascotto <panoramic56@proton.me>
2026-08-02 18:13:34 -0600

    Intiial app commit

---
+++ TinyRadar/TinyRadar Watch App/TinyRadarApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinyRadarApp.swift
+//  TinyRadar Watch App
+//
+//  Created by Bruno Gomes Pascotto on 8/2/26.
+//
+
+import SwiftUI
+
+@main
+struct TinyRadar_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TinyRadar/TinyRadar Watch AppTests/TinyRadar_Watch_AppTests.swift
@@ -0,0 +1,19 @@
+//
+//  TinyRadar_Watch_AppTests.swift
+//  TinyRadar Watch AppTests
+//
+//  Created by Bruno Gomes Pascotto on 8/2/26.
+//
+
+import Testing
+@testable import TinyRadar_Watch_App
+
+struct TinyRadar_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
